fb61f44578dc2a983fcfaf3e215c3d39f2e66d74
F1aki <159679197+F1aki@users.noreply.github.com>
2024-12-08 16:25:51 +0500

    Fishing without animations

--- Assets/activites(prefabs + scripts)/Fish/textfish.cs
+++ Assets/activites(prefabs + scripts)/Fish/textfish.cs
@@ -7,8 +7,12 @@ public class textfish : MonoBehaviour
 {
     public cameraforfishing CMF;
     public TMP_Text textComponent;
+    public TMP_Text textComponent2;
     public void FixedUpdate()
     {
         textComponent.text = "Attempts left: " + CMF.attemptsRemaining;
+        int timeregen2 = (int)CMF.timeregen;
+        string v = "Time to recovery: " + timeregen2 + "s";
+        textComponent2.text = v;
     }
 }
